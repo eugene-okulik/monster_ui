b1649e0ba096ae81a9d8665b6f2d15042782e4e3
Eugeny Okulik <tiburon854@gmail.com>
2023-09-21 20:43:15 +0300

    added allure

--- conftest.py
+++ conftest.py
@@ -4,6 +4,7 @@ from time import sleep
 from pages.sale_page import SalePage
 from pages.login_page import LoginPage
 from selenium.webdriver.chrome.options import Options
+import random
 
 
 @pytest.fixture()
@@ -11,8 +12,10 @@ def driver():
     options = Options()
     options.add_argument('--headless')
     chrome_driver = webdriver.Chrome(options=options)
-    sleep(3)
-    return chrome_driver
+    chrome_driver.maximize_window()
+    # sleep(3)
+    yield chrome_driver
+    # chrome_driver.save_screenshot(f'{random.randint(1, 5000)}.png')
 
 
 @pytest.fixture()
